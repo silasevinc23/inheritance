57c593c2389310ffe0747d211327c1ff1e4faf04
silasevinc <silasevinc23@gmail.com>
2024-08-21 11:05:35 +0300

    Update Program.cs

--- inheritance/Program.cs
+++ inheritance/Program.cs
@@ -3,10 +3,10 @@ Ogrenci ogrenci1 = new Ogrenci();
 ogrenci1._ad = "Ali";
 ogrenci1._soyad = "Kaya";
 ogrenci1._ogrenci_no = 123456;
-ogrenci1.Bilgiler();
+ogrenci1.Ogrenci_Bilgiler();
 
 Ogretmen ogretmen1 = new Ogretmen();
 ogretmen1._ad = "Salih";
 ogretmen1._soyad = "Şahin";
 ogretmen1._maas_bilgisi = 50000;
-ogretmen1.Bilgiler();
+ogretmen1.Ogretmen_Bilgiler();
